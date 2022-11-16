cec929075797c39f414cefeb2036c92f256216df
Faustina Vidal <faustinavidal98@gmail.com>
2022-11-15 23:59:44 -0300

    modificando index

--- pages/musica/js/canciones.js
+++ pages/musica/js/canciones.js
@@ -1,6 +1,33 @@
+const btnOrdenarMeGustas = document.querySelector("button#ordenarCancionesMg")
+
+const btnOrdenarMeGustasInverso = document.querySelector("button#ordenarCancionesMg")
+
 const btnOrdenarCanciones = document.querySelector("button#ordenarCancionesNombre")
 
 const btnOrdenarCancionesInverso = document.querySelector("button#ordenarCancionesNombre")
+
+//-------------Ordenar por me gustas-------------
+const ordenarPormeGusta = () => {
+    canciones.sort((elementa, elementb) => {
+        if (elementa.meGusta > elementb.meGusta) {
+            return 1
+        }
+        if (elementa.meGusta < elementb.meGusta) {
+            return -1
+        }
+        return 0
+    })
+    colocandoTarjetasMusicaConJSON()
+}
+
+btnOrdenarMeGustas.onclick = ordenarPormeGusta
+//-------------Ordenar por me gustas inverso-------------
+const ordenarPormeGustaInverso = () => {
+    canciones.reverse().meGusta
+    colocandoTarjetasMusicaConJSON()
+}
+
+btnOrdenarMeGustasInverso.addEventListener("dblclick",ordenarPormeGustaInverso)
 //-------------Ordenar por nombre-------------
 const ordenarPorCancion = () => {
     canciones.sort((elementa, elementb) => {
