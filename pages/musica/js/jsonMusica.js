f0363265906f7ac835f9262114a258bde3cb236f
Faustina Vidal <faustinavidal98@gmail.com>
2022-11-16 16:28:39 -0300

    pasando datos de tienda a json

--- pages/musica/js/jsonMusica.js
+++ pages/musica/js/jsonMusica.js
@@ -3,15 +3,18 @@ const URL = "./bbdd/canciones.json"
 
 const colocandoTarjetasMusicaConJSON = async () => {
     let armoHTML = ""
-        try {
-            const response = await fetch(URL)
-                canciones = await response.json()
-                canciones.forEach(cancion => armoHTML += armandoTarjetasMusica(cancion));
-        } catch (error) {
-            armoHTML = armandoTarjetasMusicaError()
-        } finally {
+    try {
+        const response = await fetch(URL)
+        canciones = await response.json()
+        canciones.forEach(cancion => armoHTML += armandoTarjetasMusica(cancion));
+    } catch (error) {
+        armoHTML = armandoTarjetasMusicaError()
+    } finally {
+        tarjetasMusica.innerHTML = homero()
+        setTimeout(() => {
             tarjetasMusica.innerHTML = armoHTML
-        }
+        }, 2000);
+    }
 }
 
 colocandoTarjetasMusicaConJSON()
--- pages/tienda/js/jsonTienda.js
+++ pages/tienda/js/jsonTienda.js
@@ -3,14 +3,31 @@ const URL = "./bbdd/productos.json"
 
 const colocandoTarjetasTiendaConJSON = async () => {
     let armoHTML = ""
-        try {
-            const response = await fetch(URL)
-                productos = await response.json()
-                productos.forEach(producto => armoHTML += armandoTarjetasTienda(producto));
-        } catch (error) {
-            armoHTML = armandoTarjetasTiendaError()
-        } finally {
+    let activoBtnAñadirAlCarrito = true
+    try {
+        const response = await fetch(URL)
+        productos = await response.json()
+        productos.forEach(producto => armoHTML += armandoTarjetasTienda(producto));
+    } catch (error) {
+        armoHTML = armandoTarjetasTiendaError()
+    } finally {
+        tarjetasTienda.innerHTML = homero()
+        setTimeout(() => {
             tarjetasTienda.innerHTML = armoHTML
-        }
+        }, 2000);
+    }
 }
 colocandoTarjetasTiendaConJSON()
+
+/*
+const colocandoTarjetasTienda = () => {
+    tarjetasTienda.innerHTML = homero()
+    setTimeout(() => {
+        tarjetasTienda.innerHTML = ""
+        productos.length > 0 && productos.forEach(elemento => tarjetasTienda.innerHTML += armandoTarjetasTienda(elemento))
+        activarBtnAñadirAlCarrito()
+    }, 2000);
+}
+
+colocandoTarjetasTienda()
+*/
